0a7695ceec21a35067ea43fabf6bb6e4dfca27d6
Rohit Panjwani <rohitp21.rp@gmail.com>
2018-03-11 16:59:03 +0530

    dropdown and checkcombobox added

--- src/sample/arochem/ui/customersetup/CustomerSetupController.java
+++ src/sample/arochem/ui/customersetup/CustomerSetupController.java
@@ -4,11 +4,16 @@ package sample.arochem.ui.customersetup;
 import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXTextArea;
 import com.jfoenix.controls.JFXTextField;
+import javafx.application.Application;
 import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import org.controlsfx.control.CheckComboBox;
+import org.controlsfx.control.textfield.TextFields;
 import sample.arochem.util.database.Database;
 
 import javax.xml.crypto.Data;
@@ -35,10 +40,13 @@ public class CustomerSetupController {
     private JFXTextField phoneno2;
 
     @FXML
-    private JFXTextField pincode;
+    private JFXTextField address1;
 
     @FXML
-    private JFXTextArea address;
+    private JFXTextField address2;
+
+    @FXML
+    private JFXTextField pincode;
 
     @FXML
     private JFXTextField contactperson;
@@ -71,9 +79,68 @@ public class CustomerSetupController {
     private JFXTextField web;
 
     @FXML
+    private CheckComboBox<String> product;
+
+
+    @FXML
     void handlecancelaction(ActionEvent event) {
-      //  courierpref.setItems(ol);
-       // TextFields.bindAutoCompletion(courierpref.getEditor(), courierpref.getItems());
+        Stage tobeclose= (Stage)firmname.getScene().getWindow();
+        tobeclose.close();
+    }
+
+    @FXML
+    public void initialize () {
+
+        initProduct();
+        initDropdowns();
+    }
+
+    private void initDropdowns() {
+        courierpref.setItems(ol);
+        TextFields.bindAutoCompletion(courierpref.getEditor(), courierpref.getItems());
+    }
+    private void initProduct() {
+        product.getItems().addAll(ol);
+        product.getItems().add(0,"ALL");
+        product.getItems().add(1,"NONE");
+        product.getCheckModel().getCheckedItems().addListener(new ListChangeListener<String>() {
+            boolean changing = false;
+
+            @Override
+            public void onChanged(ListChangeListener.Change<? extends String> c) {
+                if (!changing && product.getCheckModel().isChecked(0)) {
+                    // trigger no more calls to checkAll when the selected items are modified by checkAll
+                    changing = true;
+                    product.getCheckModel().check(1);
+                    product.getCheckModel().clearCheck(1);
+                    for(int i = 2;i<=6;i++)
+                        product.getCheckModel().check(i);
+                    changing = false;
+                    product.getCheckModel().clearCheck(0);
+                }
+                if (!changing && product.getCheckModel().isChecked(1)) {
+                    // trigger no more calls to checkAll when the selected items are modified by checkAll
+                    changing = true;
+                    product.getCheckModel().check(0);
+                    product.getCheckModel().clearChecks();
+                    changing = false;
+                }
+                for(int i=2;i<=6;i++) {
+                    if (!changing && product.getCheckModel().isChecked(i)) {
+                        // trigger no more calls to checkAll when the selected items are modified by checkAll
+                        changing = true;
+                        product.getCheckModel().check(1);
+                        product.getCheckModel().clearCheck(1);
+                        changing = false;
+                    }
+                }
+            }
+        });
+    }
+
+    @FXML
+    void handlepincodeaction(ActionEvent event) {
+        String pincodeans = pincode.getText();
     }
 
     @FXML
@@ -87,21 +154,29 @@ public class CustomerSetupController {
         String cellnoans = cellno.getText();
         String crmans = crm.getText();
         String contactpersonans = contactperson.getText();
-        String addressans = address.getText();
+        String addressans = address1.getText();
+        addressans = addressans + address2.getText();
         String pincodeans = pincode.getText();
         String phoneno2ans = phoneno2.getText();
         String faxans = fax.getText();
         String gstnoans = gstno.getText();
-//        String courierprefans = courierpref.getSelectionModel().getSelectedItem().toString();
-//        String stateans = state.getSelectionModel().getSelectedItem().toString();
-//        String cityans = city.getSelectionModel().getSelectedItem().toString();
+        if(courierpref.getSelectionModel().getSelectedItem()!=null){
+            String courierprefans = courierpref.getSelectionModel().getSelectedItem().toString();
+        }
+        if(state.getSelectionModel().getSelectedItem()!=null){
+            String stateans = state.getSelectionModel().getSelectedItem().toString();
+        }
+        if(city.getSelectionModel().getSelectedItem()!=null){
+            String cityans = city.getSelectionModel().getSelectedItem().toString();
+        }
 
 
-        con = Database.getConnection();
-        st = Database.getStatement(con);
 
-        if(Database.doesTableExist("CustomerSetup2"))
-            Database.createTableCustomerSetup();
+//        con = Database.getConnection();
+//        st = Database.getStatement(con);
+//
+//        if(Database.doesTableExist("CustomerSetup2"))
+//            Database.createTableCustomerSetup();
 
 //        String query = "INSERT INTO Trial Values ('"+firmnameans+"', '"+gstnoans+"', '"+courierprefans+ "', " +
 //                        "'"+phoneno2ans+"', '"+addressans+"', '"+faxans+"', '"+pincodeans+"'," +
