9531f9103165f3d36b91b61f2658697590d432f1
Rohit Panjwani <rohitp21.rp@gmail.com>
2018-03-30 20:14:45 +0530

    sampleissue transactionlist working

--- src/sample/arochem/ui/sampleissuetransactionlist/SampleIssueTransactionListController.java
+++ src/sample/arochem/ui/sampleissuetransactionlist/SampleIssueTransactionListController.java
@@ -2,6 +2,7 @@ package sample.arochem.ui.sampleissuetransactionlist;
 
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXTextField;
 import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
@@ -11,15 +12,22 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 import org.controlsfx.control.textfield.TextFields;
+import sample.arochem.ui.couriersetup.CourierSetupController;
+import sample.arochem.util.database.Database;
 import sample.arochem.util.database.DatabaseHandler;
 
 import javax.swing.*;
+import javax.xml.crypto.Data;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class SampleIssueTransactionListController {
 
-    ObservableList<String> ol = FXCollections.observableArrayList("City", "Courier Name", "Ref No");
+    ObservableList<String> ol = FXCollections.observableArrayList("refno", "refdate", "consignee", "city", "couriername", "docketno");
+    ObservableList<Sample> list = FXCollections.observableArrayList();
 
     @FXML
     private JFXButton add;
@@ -43,22 +51,25 @@ public class SampleIssueTransactionListController {
     private TableView<Sample> tableview;
 
     @FXML
-    private TableColumn<?, ?> refNo;
+    private TableColumn<Sample, String> refNo;
 
     @FXML
-    private TableColumn<?, ?> refDate;
+    private TableColumn<Sample, String> refDate;
 
     @FXML
-    private TableColumn<?, ?> consignee;
+    private TableColumn<Sample, String> consignee;
 
     @FXML
-    private TableColumn<?, ?> city;
+    private TableColumn<Sample, String> city;
 
     @FXML
-    private TableColumn<?, ?> courierName;
+    private TableColumn<Sample, String> courierName;
 
     @FXML
-    private TableColumn<?, ?> docketNo;
+    private TableColumn<Sample, String> docketNo;
+
+    @FXML
+    private JFXTextField search;
 
     @FXML
     void handlecloseaction(ActionEvent event) {
@@ -76,25 +87,77 @@ public class SampleIssueTransactionListController {
         TextFields.bindAutoCompletion(searchBy.getEditor(), searchBy.getItems());
     }
 
-    DatabaseHandler databaseHandler;
-
     @FXML
     private void initialize() {
-        databaseHandler = new DatabaseHandler();
         tableview.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
         initDropdowns();
+        initCol();
+    }
+
+    private void initCol() {
+        refNo.setCellValueFactory(new PropertyValueFactory<>("refNo"));
+        refDate.setCellValueFactory(new PropertyValueFactory<>("refDate"));
+        city.setCellValueFactory(new PropertyValueFactory<>("city"));
+        consignee.setCellValueFactory(new PropertyValueFactory<>("consignee"));
+        docketNo.setCellValueFactory(new PropertyValueFactory<>("docketNo"));
+        courierName.setCellValueFactory(new PropertyValueFactory<>("courierName"));
     }
 
     @FXML
     void handleEditOption(ActionEvent event) {
         Sample selectedForDeletion = tableview.getSelectionModel().getSelectedItem();
         if (selectedForDeletion == null) {
-            JOptionPane.showMessageDialog(null,"");
+            JOptionPane.showMessageDialog(null,"Please Select a entry");
             return;
 
         }
+    }
 
+    @FXML
+    void handlesearchaction(ActionEvent event) {
+        list.clear();
+        tableview.getItems().clear();
+        Database.getConnection();
+        Database.getStatement();
 
+        if(!Database.doesTableExist("SampleIssueTrial"))
+            Database.createTableSampleIssue();
+
+        String query = "SELECT * from SampleIssueTrial where upper("+searchBy.getSelectionModel().getSelectedItem()+") = '"+search.getText().toUpperCase()+"'";
+
+        ResultSet rs = Database.getdata(query);
+
+        Boolean isdata = false;
+
+        try {
+            while(rs.next()) {
+                    isdata = true;
+                    String refnoans = rs.getString("refno");
+                    String refdate = rs.getString("refdate");
+                    String city = rs.getString("city");
+                    String consignee = rs.getString("consignee");
+                    String docketno = rs.getString("docketno");
+                    String courier = rs.getString("couriername");
+                    list.add(new Sample(refnoans, refdate, consignee, city, courier, docketno));
+            }
+        } catch (SQLException e) {
+                e.printStackTrace();
+        }
+
+        if(!isdata) {
+            JOptionPane.showMessageDialog(null,"No Such Data");
+        }
+
+        tableview.getItems().setAll(list);
+    }
+
+    @FXML
+    void handlesearchbyaction(ActionEvent event) {
+        String ans = searchBy.getSelectionModel().getSelectedItem();
+        search.setPromptText("Enter "+ans);
+
+        if(ans.equalsIgnoreCase("refdate"))
+            search.setPromptText("YYYY-MM-DD");
     }
 
     public static class Sample {
@@ -116,28 +179,27 @@ public class SampleIssueTransactionListController {
         }
 
         public String getRefNo() {
-            return refNo.get();
+            return refNo.getValue();
         }
 
         public String getRefDate() {
-            return refDate.get();
+            return refDate.getValue();
         }
 
         public String getConsignee() {
-            return consignee.get();
+            return consignee.getValue();
         }
 
         public String getCity() {
-            return city.get();
+            return city.getValue();
         }
 
         public String getCourierName() {
-            return courierName.get();
+            return courierName.getValue();
         }
 
         public String getDocketNo() {
-            return docketNo.get();
+            return docketNo.getValue();
         }
-
     }
 }
