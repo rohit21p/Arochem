236259ad2c3c1cd8c78a59aef88bc8db34614c24
Rohit Panjwani <rohitp21.rp@gmail.com>
2018-03-30 20:14:59 +0530

    Sample issue working

--- src/sample/arochem/ui/sampleissue/SampleIssueController.java
+++ src/sample/arochem/ui/sampleissue/SampleIssueController.java
@@ -2,10 +2,22 @@ package sample.arochem.ui.sampleissue;
 
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXDatePicker;
 import com.jfoenix.controls.JFXTextField;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.stage.Stage;
+import org.controlsfx.control.textfield.TextFields;
+import sample.arochem.util.database.Database;
+
+import javax.swing.*;
+import java.time.LocalDate;
 
 public class SampleIssueController {
+
+    ObservableList<String> ol = FXCollections.observableArrayList("Rohit","Namit","Raj kale","Rohan","Praveen");
 
     @FXML
     private JFXTextField Consignee;
@@ -20,6 +32,9 @@ public class SampleIssueController {
     private JFXTextField CRM;
 
     @FXML
+    private JFXComboBox<String> CourierName;
+
+    @FXML
     private JFXTextField RefNo;
 
     @FXML
@@ -32,7 +47,10 @@ public class SampleIssueController {
     private JFXTextField City;
 
     @FXML
-    private JFXComboBox<?> CourierName;
+    private JFXDatePicker todaysdate;
+
+    @FXML
+    private JFXDatePicker nextdate;
 
     @FXML
     private JFXButton Save;
@@ -42,5 +60,51 @@ public class SampleIssueController {
 
     @FXML
     private JFXButton Close;
+
+    @FXML
+    void initialize() {
+        Database.getConnection();
+        Database.getStatement();
+        LocalDate date = LocalDate.now();
+        todaysdate.setValue(date);
+        nextdate.setValue(date.plusDays(7));
+        initDropdowns();
+    }
+
+    @FXML
+    void handlecancelaction(ActionEvent event) {
+        Stage stage = (Stage)RefNo.getScene().getWindow();
+        stage.close();
+    }
+
+    @FXML
+    void handleprintaction(ActionEvent event) {
+
+    }
+
+
+    private void initDropdowns() {
+        CourierName.setItems(ol);
+        TextFields.bindAutoCompletion(CourierName.getEditor(), CourierName.getItems());
+    }
+
+    @FXML
+    void handlesaveaction(ActionEvent event) {
+        Database.getConnection();
+        Database.getStatement();
+
+        LocalDate date = todaysdate.getValue();
+        String dateans = String.valueOf(date);
+        String couriernameans = "";
+        if(CourierName.getSelectionModel().getSelectedItem()!=null)
+            couriernameans = CourierName.getSelectionModel().getSelectedItem().toString();
+        String docketnoans = "";
+
+        if(!Database.doesTableExist("SampleIssueTrial"))
+            Database.createTableSampleIssue();
+
+        String query = "INSERT INTO SampleIssueTrial values('"+RefNo.getText()+"','"+dateans+"','"+Consignee.getText()+"','"+City.getText()+"','"+couriernameans+"','"+docketnoans+"')";
+        Database.enterIntoDB(query);
+    }
 
 }
